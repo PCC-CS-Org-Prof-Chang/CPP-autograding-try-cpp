2ee11696f4890a8225283619939157c0ccc5eb35
Ken Chang - CS Professor <154487739+adjprof2@users.noreply.github.com>
2024-09-29 12:06:38 -0700

    Update student.hpp

--- student.hpp
+++ student.hpp
@@ -5,8 +5,17 @@
 // Exercise 1: implements function 'add', that adds two numbers
 int add(int a, int b);
 
-// Exercise 2: implements function 'mul', that multiplies two numbers
+// Exercise 2: implements function 'subtract', that subtract two numbers
+int subtract(int a, int b);
+
+// Exercise 3: implements function 'mul', that multiplies two numbers
 int mul(int a, int b);
+
+// Exercise 4: implements function 'div', that divides two numbers and return int
+int div(int a, int b);
+
+// Exercise 5: implements function 'div', that divides two double numbers and return double
+double div(double a, double b);
 
 // ===============
 // implementations
@@ -16,8 +25,23 @@ int add(int a, int b)
    return 0; // TODO: fix
 }
 
-// Exercise 2: implements function 'mul', that multiplies two numbers
+int subtract(int a, int b)
+{
+   return 0; // TODO: fix
+}
+
 int mul(int a, int b)
 {
    return 0; // TODO: fix
 }
+
+int div(int a, int b)
+{
+   return 0; // TODO: fix
+}
+
+double div(double a, double b)
+{
+   return 0; // TODO: fix
+}
+
